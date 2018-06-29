f45852f87574be0e8f1539f38d3fdcfa86183b68
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2018-06-29 14:33:46 -0300

    UPDATE - Atualizando página inicial com menu

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
-import { ProdutosListComponent } from './produtos/produtos-list/produtos-list.component';
 
 import { AppRoutingModule } from './app.routing';
 import { ProdutosModule } from './produtos/produtos.module';
-import { ProdutoListRouting } from './produtos/produtos.routing';
-import { ProdutosInsertComponent } from './produtos/produtos-insert/produtos-insert.component';
 
 
 @NgModule({
